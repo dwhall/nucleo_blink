774c882061520d6fe6fddea7c00a472773112704
!!Dean <dwhall256@gmail.com>
2023-05-31 21:47:29 -0500

    main() that might blink builds

--- src/main.nim
+++ src/main.nim
@@ -1,1 +1,11 @@
-let a = "pickles"
+import stm32f446
+
+proc main() =
+  modifyIt(RCC.AHB1ENR): it.GPIOAEN= true
+  modifyIt(GPIOA.MODER): it.MODER5= 1
+  while true:
+    GPIOA.BSRR.write(BS5=true)
+    GPIOA.BSRR.write(BR5=true)
+
+when isMainModule:
+  main()
