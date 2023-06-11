0c42e52fe52efc37d668cbc4ebfe6eeace6be188
!!Dean <dwhall256@gmail.com>
2023-06-11 17:45:29 -0500

    Manual write(constant.GPIOA_BSRR_Fields) --> 3.2 MHz

--- src/main.nim
+++ src/main.nim
@@ -4,8 +4,8 @@ proc main() =
   modifyIt(RCC.AHB1ENR): it.GPIOAEN= true
   modifyIt(GPIOA.MODER): it.MODER5= 1
   while true:
-    GPIOA.BSRR.write(BS5=true)
-    GPIOA.BSRR.write(BR5=true)
+    GPIOA.BSRR.write((1 shl 5).GPIOA_BSRR_Fields)
+    GPIOA.BSRR.write((1 shl 21).GPIOA_BSRR_Fields)
 
 when isMainModule:
   main()
